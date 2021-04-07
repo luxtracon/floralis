34dea528d9f8e3bbd6be7faf6c4e44181d7ffae1
luxtracon <luxtracon@web.de>
2021-04-07 16:33:53 +0200

    update to version 1.2.0

--- src/main/java/com/luxtracon/floralis/utilities/Reference.java
+++ src/main/java/com/luxtracon/floralis/utilities/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MODID = "floralis";
 	public static final String MODNAME = "Floralis";
-	public static final String MODVERSION = "1.1.0";
+	public static final String MODVERSION = "1.2.0";
 	public static final String MODDEPENDENCIES = "after:forestry;"+"after:immersiveengineering;"+"after:industrialforegoing;"+"after:thermalexpansion";
 }
